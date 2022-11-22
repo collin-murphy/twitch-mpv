c1c848392de5ff87efef04ac4022b11f85dbf4ec
Collin Murphy <collinmuurphy@gmail.com>
2022-11-22 12:45:53 -0800

    added comments
    comments were added, README was updated, and the 'streamer' variable
    name was changed to 'channel'

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # twitch-mpv
+## Open twitch streams on the command line and skip the ads
+Usage: `python3 twitch.py CHANNEL_NAME`
+
+## To do
+- can't be run as script 
+- save streamers
+- viewcount/title/category support
+
--- twitch.py
+++ twitch.py
@@ -4,41 +4,44 @@ from subprocess import DEVNULL, STDOUT, check_call
 from bs4 import BeautifulSoup
 import requests as rq
 
+#check if channel is live logic
 def check_live(URL: str):
     request = rq.get(URL).content.decode('utf-8')
     return ('isLiveBroadcast' in request)
 
-def handle_live(URL: str, streamer: str):
+#handle what happens from check_live logic
+def handle_live(URL: str, channel: str):
+    #if channel is live, open
     if check_live(URL) == True:
-        print(f'Opening {streamer}\'s stream.')
+        print(f'Opening {channel}\'s stream @ {URL}')
         open_mpv(URL)
+    #if not, tell user
     else:
-        print(f'{streamer} is not live.')
+        print(f'{channel} is not live.')
 
-
+#open file
 def open_mpv(twitch_link_full:str):
-    print(f'Opening {twitch_link_full}') 
     check_call(['mpv', twitch_link_full], stdout=DEVNULL, stderr=STDOUT)
 
-    
+#executed on script run    
 def main():
-    arg_count = len(sys.argv)
-     
-    streamer = ""
+    #get channel
+    arg_count = len(sys.argv) 
+    channel = ""
     if arg_count == 2:
-        streamer = sys.argv[1]
+        channel = sys.argv[1]
     elif arg_count == 1:
-        streamer = input('type a streamer name:')
+        channel = input('Type a channel name:')
     else:
-        print("Usage: python3 twitch.py STREAMER-NAME")
+        print("Usage: python3 twitch.py CHANNEL_NAME")
         return
 
-    link = f'http://www.twitch.tv/{streamer}'
+    channel = channel.lower()
+    #generate twitch link
+    link = f'http://www.twitch.tv/{channel}'
 
-    handle_live(link, streamer)    
-    
-
-    #open_mpv(link)
+    #check if channel is live
+    handle_live(link, channel)    
 
 
 if __name__ == "__main__":
